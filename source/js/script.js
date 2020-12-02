828c0efd6a4d0d133c2ac737cdbbf8279b7e956d
Dmitry Lekomtsev <dklekomtsev@yandex.ru>
2020-12-02 20:52:08 +0300

    feat: module 1, task 2

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import handlePageLoad from './modules/handle-page-load';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+handlePageLoad();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
